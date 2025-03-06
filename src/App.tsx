eb8257e33f5037ea935b6ee1d4163f507fc461d3
user-1pa <82113453+user-1pa@users.noreply.github.com>
2025-03-06 00:44:58 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "@/contexts/CartContext";
+
+// Pages
 import Index from "./pages/Index";
 import Shop from "./pages/Shop";
+import ProductDetail from "./pages/ProductDetail";
 import Services from "./pages/Services";
 import Events from "./pages/Events";
 import Media from "./pages/Media";
@@ -14,6 +17,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
+import Checkout from "./pages/Checkout"; // We'll create this next
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -22,20 +26,24 @@ const App = () => {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/media" element={<Media />} />
-            <Route path="/testimonials" element={<Testimonials />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+          <CartProvider>
+            <Toaster />
+            <Sonner />
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/shop/:id" element={<ProductDetail />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/media" element={<Media />} />
+              <Route path="/testimonials" element={<Testimonials />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </CartProvider>
         </TooltipProvider>
       </QueryClientProvider>
     </BrowserRouter>
